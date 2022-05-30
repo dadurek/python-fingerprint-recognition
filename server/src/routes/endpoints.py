55d132642e1fcac164c48e2c0f0d55a0eacfe137
Marcin Dadura <m.dadura@stud.elka.pw.edu.pl>
2022-05-30 23:16:53 +0200

    fixed Dockerfile and update sourcecode

--- server/src/routes/endpoints.py
+++ server/src/routes/endpoints.py
@@ -12,30 +12,29 @@ async def create_upload_files():
     return {"hello": "world"}
 
 
-# check if such fingerprint exist in databse (mongo)
+# check if such fingerprint exist in database directory
 @router.post("/compare-with-user")
-async def compare_with_database(upload_file: UploadFile, username: str, compare_with_all: Optional[bool] = False): # TODO handle copate_with_all parameter
-    uploaded_file_path: str = await save_file(upload_file=upload_file)
+async def compare_with_database(upload_file: UploadFile, username: str):
+    uploaded_file_path: str = await save_file(upload_file=upload_file, username=None)
     result = compare_with_user(username, uploaded_file_path)
     await remove_file(uploaded_file_path)
     return {"match": result}
 
 
-# compare two fingerpirints
+# compare two fingerprints
 @router.post("/compare")
 async def create_upload_files(upload_file_1: UploadFile, upload_file_2: UploadFile):
-    uploaded_file_path_1: str = await save_file(upload_file=upload_file_1)
-    uploaded_file_path_2: str = await save_file(upload_file=upload_file_2)
+    uploaded_file_path_1: str = await save_file(upload_file=upload_file_1, username=None)
+    uploaded_file_path_2: str = await save_file(upload_file=upload_file_2, username=None)
     score = compare_fingerprints(uploaded_file_path_1, uploaded_file_path_2)
     await remove_file(uploaded_file_path_1)
     await remove_file(uploaded_file_path_2)
     return {"score": score}
 
 
-# @router.post("/new-user")
-# async def register_new_user(upload_files: list[UploadFile], username: str):
-#     uploaded_files = []
-#     for file in upload_files:
-#         uploaded_files.append(await save_file(upload_file=file, base_directory=username))
-#     return {"filenames": [file.filename for file in upload_files]}
+# add new user with fingerprint or random photo :D
+@router.post("/add-file")
+async def register_new_user(upload_file: UploadFile, username: str):
+    await save_file(upload_file=upload_file, username=username)
+    return {"message": 'OK'}
 
